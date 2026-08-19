443362cdc7335ce570487768b777996a5bcff99c
Eric Lima <erpdl2@gmail.com>
2026-08-19 12:56:23 -0300

    feat: add Google Tag Manager as the single analytics entry point

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,10 @@ import "./globals.css"
 import { CHURCH_SITE_URL } from "@/const"
 import ChurchJsonLd from "@/components/seo/ChurchJsonLd"
 import WebSiteJsonLd from "@/components/seo/WebSiteJsonLd"
+import {
+  GoogleTagManagerScript,
+  GoogleTagManagerNoScript,
+} from "@/components/analytics/GoogleTagManager"
 
 const cormorantGaramond = Cormorant_Garamond({
   subsets: ["latin"],
@@ -73,6 +77,8 @@ export default function RootLayout({
       <body
         className={`${cormorantGaramond.variable} ${raleway.variable} ${jetBrainsMono.variable} ${tangerine.variable} antialiased`}
       >
+        <GoogleTagManagerNoScript />
+        <GoogleTagManagerScript />
         <ChurchJsonLd />
         <WebSiteJsonLd />
         {children}
